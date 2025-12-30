a0b670549287672b31cdb699c1f5fee3f5d53f30
Rajendra Mahapatra <rajendramahapatra786@gmail.com>
2025-12-30 23:36:49 +0530

    Updated game files

--- README.md
+++
@@ -1,1 +0,0 @@
-A fun game project
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function play(userChoice) {
-    const options = ["snake", "water", "gun"];
+    const options = ["snake", "water", "Sniper"];
     const computerChoice = options[Math.floor(Math.random() * 3)];
 
     let result = `You chose ${userChoice} <br> Computer chose ${computerChoice} <br>`;
@@ -8,8 +8,8 @@ function play(userChoice) {
         result += "🤝 It's a Draw!";
     } else if (
         (userChoice === "snake" && computerChoice === "water") ||
-        (userChoice === "water" && computerChoice === "gun") ||
-        (userChoice === "gun" && computerChoice === "snake")
+        (userChoice === "water" && computerChoice === "Sniper") ||
+        (userChoice === "Sniper" && computerChoice === "snake")
     ) {
         result += "🎉 You Win!";
     } else {
